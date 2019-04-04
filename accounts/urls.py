424da4d8a70a091683b70cdc543183565dda1635
shubham7298 <shubhamsingh7298@gmail.com>
2019-04-04 23:52:06 +0530

    Signup & Login

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from .views import home, register
+
+urlpatterns = [
+	path('login/',auth_views.LoginView.as_view(), name='login'),
+	# path('logout/', auth_views.LogoutView.as_view(template_name = 'registration/logged_out.html'), name='logout'),
+	path('logout/', auth_views.LogoutView.as_view(), {'template_name': 'registration/logged_out.html'}, name='logout'),
+	path('',home),
+	path('register/',register)
+]
